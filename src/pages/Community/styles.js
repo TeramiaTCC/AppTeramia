fe8623c97746d9c82cc8b49b465b3fd727044b82
Teramia-TCC <maldosostcc@gmail.com>
2023-10-07 14:41:47 -0300

    Buttons

--- src/pages/Community/styles.js
+++ src/pages/Community/styles.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F16520',
     position: 'absolute',
     bottom: 80,
-    left: 20,
+    right: 20,
     borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
